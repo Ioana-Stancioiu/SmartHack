2ac322e0f80b8fd05355b95754393692df1ac09f
MarianNoaghea <noaghea.marian@gmail.com>
2021-11-06 14:37:23 +0200

    msj

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -2,7 +2,21 @@ package com.company;
 
 import javax.swing.*;
 
-public class MainFrame {
-    private JButton button1;
+public class MainFrame extends JFrame {
+    private JButton Login;
     private JTextField tf;
+    private JTextField tf2;
+    private JPanel mainPanel;
+
+    public MainFrame() {
+        setTitle("Manager de parole");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setVisible(true);
+        setContentPane(mainPanel);
+    }
+
+    public static void main(String[] args) {
+        MainFrame myFrame = new MainFrame();
+    }
 }
